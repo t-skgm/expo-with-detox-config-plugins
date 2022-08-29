75916d5eec1300003e42769de7f83a232fd98718
t-skgm <skgm0301@gmail.com>
2022-08-29 16:38:27 +0900

    copy detoxrc from exp repo

---
+++ detox.config.js
@@ -0,0 +1,53 @@
+const iosName = "expodetoxconfigplugins";
+const emulator = "Pixel_3a_API_30";
+const simulator = "iPhone 13 Pro";
+const derivedDataPath = "ios/build";
+const sdk = "iphonesimulator";
+
+module.exports = {
+  testRunner: "jest",
+  runnerConfig: require.resolve("./e2e/jest.config.js"),
+  specs: "e2e",
+  behavior: {
+    init: {
+      exposeGlobals: false,
+    },
+  },
+  apps: {
+    "ios.debug": {
+      type: "ios.app",
+      binaryPath: `${derivedDataPath}/Build/Products/Debug-${sdk}/${iosName}.app`,
+      build: `./scripts/start-metro.sh && ./scripts/build-detox-ios.sh ${iosName} Debug`,
+    },
+    "android.debug": {
+      type: "android.apk",
+      binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
+      build:
+        "./scripts/start-metro.sh && pushd android; ./gradlew app:assembleDebug app:assembleAndroidTest -DtestBuildType=debug; popd",
+    },
+  },
+  devices: {
+    simulator: {
+      type: "ios.simulator",
+      device: {
+        type: simulator,
+      },
+    },
+    emulator: {
+      type: "android.emulator",
+      device: {
+        avdName: emulator,
+      },
+    },
+  },
+  configurations: {
+    "ios.sim.debug": {
+      device: "simulator",
+      app: "ios.debug",
+    },
+    "android.emu.debug": {
+      device: "emulator",
+      app: "android.debug",
+    },
+  },
+};
